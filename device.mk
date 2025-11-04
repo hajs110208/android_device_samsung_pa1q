7b8c7092694e317d30b1cd6bb5bfb326ac146ecf
Maxim Trusov <146969829+Maxim-Root@users.noreply.github.com>
2025-11-04 16:06:49 +0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -48,9 +48,7 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# OEM otacerts
-PRODUCT_EXTRA_RECOVERY_KEYS += \
-    $(DEVICE_PATH)/security/otacert
+
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
